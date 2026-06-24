ffe0d18125c6bc875c352bdf597ecdaefd332e0c
Kiber2009 <92222946+Kiber2009@users.noreply.github.com>
2026-06-24 18:41:50 +0300

    Add `RecipesManager`

---
+++ src/main/java/io/github/kiber2009/plugin/contentapi/RecipesManager.java
@@ -0,0 +1,12 @@
+package io.github.kiber2009.plugin.contentapi;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
+
+final class RecipesManager implements Listener {
+    @EventHandler
+    private static void onPrepareItemCraft(final PrepareItemCraftEvent event) {
+        // TODO
+    }
+}
