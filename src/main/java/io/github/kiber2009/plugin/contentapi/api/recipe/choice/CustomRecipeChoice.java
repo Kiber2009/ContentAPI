344c8e3bd4765b3831332b8d67db2024140992cf
Kiber2009 <92222946+Kiber2009@users.noreply.github.com>
2026-06-24 20:06:23 +0300

    Remove `CustomRecipeChoice`

--- src/main/java/io/github/kiber2009/plugin/contentapi/api/recipe/choice/CustomRecipeChoice.java
+++
@@ -1,8 +0,0 @@
-package io.github.kiber2009.plugin.contentapi.api.recipe.choice;
-
-import org.bukkit.inventory.ItemStack;
-import org.jspecify.annotations.Nullable;
-
-public interface CustomRecipeChoice {
-    boolean test(final @Nullable ItemStack stack);
-}
